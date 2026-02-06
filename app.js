49466ae8b322760f22a0ecf5d8d34eb1757490b1
Theodore Belo <139794007+StellarGrade-Theo@users.noreply.github.com>
2026-02-06 12:32:53 -0500

    improved errors

--- app.js
+++ app.js
@@ -61,6 +61,7 @@ app.put("/post-image", (req, res, next) => {
   if (!req.file) return res.status(200).json({ message: "No file provided" });
   if (req.body.oldPath) clearImage(req.body.oldPath);
 
+  console.log("HERE!", req);
   return res.status(201).json({
     message: "File stored",
     filePath: req.file.path,
